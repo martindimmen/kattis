691ced28a778cf16eb2caa2a5d163ab51a07da0f
MartinDimmen <martindimmen@proton.me>
2024-04-24 13:04:13 +0200

    HippHipp

--- java_solutions/HippHipp.java
+++ java_solutions/HippHipp.java
@@ -1,7 +1,6 @@
-
 public class HippHipp {
-    public static void Main(String[] args){
-        for (int i=0;i<20;i++){
+    public static void main(String[] args){
+        for (int i=0; i<20; i++){
             System.out.println("Hipp Hipp hurra!");
         }
     }
