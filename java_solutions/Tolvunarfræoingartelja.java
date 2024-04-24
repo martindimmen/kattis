aa6361424fa5dd68545aff43e23ec53d04679d15
MartinDimmen <martindimmen@proton.me>
2024-04-24 19:07:47 +0200

    Tolvunarfræoingartelja

---
+++ java_solutions/Tolvunarfræoingartelja.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Tolvunarfræoingartelja{
+    public static void main(String[] args){
+        Scanner k=new Scanner(System.in);
+        int number=k.nextInt();
+        System.err.println(number-1);
+    }
+
+}
