6ff04cc345d9b4e6b3fa44f39362ebe3318f18fe
MartinDimmen <martindimmen@proton.me>
2024-04-24 19:25:04 +0200

    firstChar

---
+++ java_solutions/LubbiLaerir.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class LubbiLaerir {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String word=s.nextLine();
+        char firstChar=word.charAt(0);
+        System.out.println(firstChar);
+    }
+}
