e605e92f06b56995ca46eaaf28c4ba677e7cd0fd
martindimmen <martindimmen@proton.me>
2025-09-09 15:47:06 +0200

    new files

---
+++ c#_solutions/Öfugsnúið/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Öfugsnúið
+{
+  class Öfugsnúið
+  {
+      static void Main()
+    {
+        int n=int.Parse(Console.ReadLine());
+        int [] numbers=new int[n];
+        for (int i=0;i<n;i++){
+            int input = int.Parse(Console.ReadLine());
+
+            numbers[i]=input;
+        }
+        for (int i=n-1 ;i>=0;i--){
+            Console.WriteLine(numbers[i]);
+        }
+    }
+  }
+}
